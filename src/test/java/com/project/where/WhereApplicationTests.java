405ed5a67dc271a49c8bf93347c58da6434fd7f1
pietro <pietro.ciani@edu.unifi.it>
2024-09-19 18:01:25 +0200

    spring init

---
+++ src/test/java/com/project/where/WhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.where;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
